498e4ad85d21f404bf7158d636dc98cbdb622685
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2021-04-13 14:18:49 +0900

    Signup layout 생성

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -3,7 +3,21 @@ import './Signup.scss';
 
 class Signup extends Component {
   render() {
-    return <div className="Signup"></div>;
+    return (
+      <div className="Signup">
+        <form className="SignupForm">
+          <h1 className="SignupTitle">
+            <em>더미트&</em> 신규 회원가입
+          </h1>
+          <input className="SignupId" placeholder="아이디"></input>
+          <input className="SignupPw" placeholder="비밀번호"></input>
+          <input className="SignupPwVerify" placeholder="비밀번호 확인"></input>
+          <input className="SignupName" placeholder="이름"></input>
+          <input className="SignupEmail" placeholder="휴대전화 번호"></input>
+          <button className="SignupBtn">가입하기</button>
+        </form>
+      </div>
+    );
   }
 }
 
