9974f8b7eb7942a68cf95c9312bb46e401320478
이재영 <2JAEYOUNG@bagchaehoui-MacBookAir.local>
2021-04-15 18:02:49 +0900

    ArticleDiscount title만 생성 nav 리뷰반영하러 가기전 commit

--- src/Pages/Main/ArticleDiscount/ArticleDiscount.js
+++ src/Pages/Main/ArticleDiscount/ArticleDiscount.js
@@ -2,7 +2,17 @@ import React, { Component } from 'react';
 import './ArticleDiscount.scss';
 class ArticleDiscount extends Component {
   render() {
-    return <div className="articleDiscount"></div>;
+    return (
+      <div className="articleDiscount">
+        <div className="articleDiscountBox">
+          <div className="discountContents">
+            <div className="discountTitle">
+              <p className="discountTitleName">할인 특가</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
