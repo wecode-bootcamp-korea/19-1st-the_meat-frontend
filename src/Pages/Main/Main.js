e708276fc6c277a30a17e0e276698d7683980d8a
이재영 <2JAEYOUNG@bagchaehoui-MacBookAir.local>
2021-04-17 23:19:52 +0900

    Nav,NavCategory 컴포넌트 분리

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Nav from '../Nav/Nav';
+import NavCategory from '../Nav/NavCategory';
 import './Main.scss';
 
 class Main extends Component {
@@ -7,6 +8,7 @@ class Main extends Component {
     return (
       <div className="Main">
         <Nav />
+        <NavCategory />
       </div>
     );
   }
--- src/Pages/Nav/Nav.js
+++ src/Pages/Nav/Nav.js
@@ -41,51 +41,8 @@ class Nav extends Component {
             </div>
           </div>
         </div>
-        <div className="navCategory">
-          <div className="categoryLeft">
-            <ul className="categoryLeftUl">
-              <i className="fas fa-bars"></i>
-              {CATEGORYLEFT.map((el, id) => (
-                <li className="categoryLeftLi" key={id}>
-                  <Link to="/" className="categoryLeftLink">
-                    {el.content}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="categoryRight">
-            <ul className="categoryRightUl">
-              {CATEGORYRIGHT.map((el, id) => (
-                <li className="categoryRightLi" key={id}>
-                  <Link to="/" className="categoryRightLink">
-                    {el.content}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
       </nav>
     );
   }
 }
-
 export default Nav;
-
-const CATEGORYLEFT = [
-  { id: 1, content: '카테고리' },
-  { id: 2, content: '할인특가' },
-  { id: 3, content: '신상품' },
-  { id: 4, content: '베스트' },
-  { id: 5, content: '더고기정육관' },
-  { id: 6, content: '이달의맛집' },
-  { id: 7, content: '7데이' },
-  { id: 8, content: '정기식단' },
-];
-
-const CATEGORYRIGHT = [
-  { id: 1, content: '로그인' },
-  { id: 2, content: '회원가입' },
-  { id: 3, content: '이벤트/혜택' },
-];
