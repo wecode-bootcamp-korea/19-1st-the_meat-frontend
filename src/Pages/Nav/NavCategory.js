62e0231f1f901ca42dfcf516a48c0c3cae673784
이재영 <2JAEYOUNG@bagchaehoui-MacBookAir.local>
2021-04-20 15:22:38 +0900

    mouseEnter mouseLeave 구현중

--- src/Pages/Nav/NavCategory.js
+++ src/Pages/Nav/NavCategory.js
@@ -5,7 +5,10 @@ import '../Nav/NavCategory.scss';
 class NavCategory extends Component {
   constructor() {
     super();
-    this.state = { categoryData: [] };
+    this.state = {
+      categoryData: [],
+      isMouse: false,
+    };
   }
 
   componentDidMount() {
@@ -18,35 +21,58 @@ class NavCategory extends Component {
       });
   }
 
+  insideCategory = () => {
+    this.setState({
+      isMouse: true,
+    });
+    console.log('inside:', this.state.isMouse);
+  };
+
+  outsideCategory = () => {
+    this.setState({
+      isMouse: !this.state.isMouse,
+    });
+    console.log('outside:', this.state.isMouse);
+  };
+
   render() {
     const { categoryData } = this.state;
+    console.log('최종isMouse:', this.state.isMouse);
     return (
       <div className="navCategory">
         <div className="categoryLeft">
           <ul className="categoryLeftUl">
             <i className="fas fa-bars"></i>
-            <li className="parentCategory">카테고리</li>
-            <div className="categoryBox">
-              {categoryData.map((category, id) => (
-                <li key={id} className="category">
-                  <img
-                    className="categoryImg"
-                    src={category.image}
-                    alt="소고기"
-                  />
-                  {category.name}
-                  <ul className="subCategoryBox">
-                    {categoryData[0].subcategory.map((subcategory, id) => (
-                      <li key={id} className="subCategory">
-                        <Link to="/" className="subCategoryLink">
-                          {subcategory.content}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ))}
-            </div>
+            <li
+              className="parentCategory"
+              onMouseEnter={this.insideCategory}
+              onMouseLeave={this.outsideCategory}
+            >
+              카테고리
+            </li>
+            {this.state.isMouse && (
+              <div className="categoryBox">
+                {categoryData.map((category, id) => (
+                  <li key={id} className="category">
+                    <img
+                      className="categoryImg"
+                      src={category.image}
+                      alt="소고기"
+                    />
+                    {category.name}
+                    <ul className="subCategoryBox">
+                      {categoryData[0].subcategory.map((subcategory, id) => (
+                        <li key={id} className="subCategory">
+                          <Link to="/" className="subCategoryLink">
+                            {subcategory.content}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                ))}
+              </div>
+            )}
             {CATEGORYLEFT.map((el, id) => (
               <li className="categoryLeftLi" key={id}>
                 <Link to="/" className="categoryLeftLink">
