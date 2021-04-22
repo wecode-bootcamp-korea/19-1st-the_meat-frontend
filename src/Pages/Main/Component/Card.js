10080e0730df66f6120c60b8570f314ba3795a4f
이재영 <2JAEYOUNG@bagchaehoui-MacBookAir.local>
2021-04-22 16:57:39 +0900

    main 마지막수정

--- src/Pages/Main/Component/Card.js
+++ src/Pages/Main/Component/Card.js
@@ -8,7 +8,7 @@ class Card extends Component {
     return (
       <div className="mdContents">
         <div className="mdImgCartBox">
-          {!!data.discount_rate && (
+          {data.discount_rate !== 0 && (
             <span className="mdSale">
               <p className="mdDiscount">{data.discount_rate} %</p>
             </span>
@@ -28,7 +28,7 @@ class Card extends Component {
             <p className="mdPrice">
               <span className="fontBold">{data.real_price}</span>원
             </p>
-            {!!data.original_price && (
+            {Number(data.original_price) !== 0 && (
               <span className="priceDiscount">{data.original_price}원</span>
             )}
           </div>
